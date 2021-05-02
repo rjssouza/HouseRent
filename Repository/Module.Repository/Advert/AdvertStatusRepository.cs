57f8919e3fb09bcd3b52a62534ce07e2df82fe7e
Robson Jesus de Souza <robson.souza@lalubema.com>
2021-05-02 19:39:45 -0300

    implementar busca para combo de status

--- Repository/Module.Repository/Advert/AdvertStatusRepository.cs
+++ Repository/Module.Repository/Advert/AdvertStatusRepository.cs
@@ -1,7 +1,10 @@
-﻿using Module.Factory.Interface.Conexao;
+﻿using Module.Dto.Base;
+using Module.Factory.Interface.Conexao;
 using Module.Repository.Base;
 using Module.Repository.Interface.Advert;
 using Module.Repository.Model.Advert;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Module.Repository.Advert
 {
@@ -9,6 +12,14 @@ namespace Module.Repository.Advert
     {
         public AdvertStatusRepository(IDbConnectionFactory dbService) : base(dbService)
         {
+        }
+
+        public IEnumerable<GenericGuidSelectDto> GetSelection()
+        {
+            var sql = new StringBuilder("select id as Value, name as Text from status  ");
+            var result = this.Select<GenericGuidSelectDto>(sql.ToString());
+
+            return result;
         }
     }
 }
